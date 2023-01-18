5049ed5254f5db42055ae9cd2cdb942d9637a85d
Nhut <vivuvivunice@gmail.com>
2023-01-18 15:58:25 +0700

    update link github

--- src/todo-app/Link.js
+++ src/todo-app/Link.js
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 export default function Link() {
   return (
     <>
-       <Button variant="outline-dark" href='https://myboo235.github.io/TODO-app/'>
+       <Button variant="outline-dark" href='https://github.com/Myboo235/TODO-app'>
         Github
         </Button> 
     </>
